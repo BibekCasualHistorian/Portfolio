500a3bb23e5d18a0029b868fb647aa2bf7cf32d1
Bibek Koirala <www.koiralabibek2058@gmail.com>
2024-10-09 17:39:06 +0545

    real

--- Server/server.js
+++ Server/server.js
@@ -9,6 +9,28 @@ dotenv.config();
 const app = express();
 const PORT = process.env.PORT || 3000;
 
+const allowedDomains = ["https://portfolio-frontend-pikm.onrender.com"];
+
+app.use((req, res, next) => {
+  const origin = req.headers.origin;
+
+  if (allowedDomains.includes(origin)) {
+    res.setHeader("Access-Control-Allow-Origin", origin);
+  }
+
+  res.header("Access-Control-Allow-Credentials", "true");
+  res.header(
+    "Access-Control-Allow-Headers",
+    "Origin,X-Requested-With,Content-Type,Accept,Authorization"
+  );
+
+  if (req.method === "OPTIONS") {
+    res.header("Access-Control-Allow-Methods", "PUT,POST,PATCH,DELETE,GET");
+    return res.status(200).json({});
+  }
+  next();
+});
+
 app.use(express.json());
 app.use(
   cors({
